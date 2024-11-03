b91385cc326d4d94f4dd2e677a6ccf0f47f50e37
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-11-03 10:51:38 -0300

    getting random card in browser

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  axios
+    .get("https://db.ygoprodeck.com/api/v7/randomcard.php")
+    .then((resp) => {
+      const card = resp.data;
+      res.send(card);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.send(e);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en el puerto 3000");
+});
